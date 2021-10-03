50857942de091d04232340f0f077b487ae2b6ebc
Karen Gasparyan <mrgasparyan92@gmail.com>
2021-10-03 01:04:51 -0700

    add Example.java

---
+++ src/lesson2/Example.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("          hello world");
+        System.out.println("Java");
+    }
+
+}
