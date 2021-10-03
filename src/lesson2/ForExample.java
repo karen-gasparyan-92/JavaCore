8e4e0280b294d5e40b5ad99d4fb44a17c5e818e8
Karen Gasparyan <mrgasparyan92@gmail.com>
2021-10-03 01:08:09 -0700

    add ForExample, changed Example

---
+++ src/lesson2/ForExample.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+public class ForExample {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
